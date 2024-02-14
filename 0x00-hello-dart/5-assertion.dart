890a7d9621ab0b26215f1e2a7435fcccd8c9d76d
timidgeek <9liveslinds@gmail.com>
2024-02-13 21:07:42 -0600

    use assert

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,12 +1,7 @@
 void main(List<String> args) {
   var number = int.parse(args[0]);
   
-  switch (number >= 80 ? 0 : 1) {
-    case 0:
-      print('You Passed');
-      break;
-    case 1:
-      print('Unhandled exception:\nUncaught Error: Assertion failed: "The score must be bigger or equal to 80');
-      break;
-  }
+  assert(number >= 80, 'The score must be bigger or equal to 80');
+
+  print('You Passed');
 }
