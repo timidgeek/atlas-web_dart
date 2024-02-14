d9d4f39b4b3748a40b3e2229d9828c5d0d102246
timidgeek <9liveslinds@gmail.com>
2024-02-13 21:01:54 -0600

    task 5

---
+++ 0x00-hello-dart/5-assertion.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  var number = int.parse(args[0]);
+  
+  switch (number >= 80 ? 0 : 1) {
+    case 0:
+      print('You Passed');
+      break;
+    case 1:
+      print('Uncaught Error: Assertion failed: "The score must be bigger or equal to 80');
+      break;
+  }
+}
