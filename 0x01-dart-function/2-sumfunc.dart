1fed2a70ec84613c4412954cc4a56d959ebf7572
timidgeek <9liveslinds@gmail.com>
2024-02-15 12:53:36 -0600

    task two check

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,18 @@
+// returns the sum and a message
+String add(int a, int b) {
+  int output = a + b;
+  return '$output, Add $a + $b = $output';
+}
+
+// returns the difference and a message
+String sub(int a, int b) {
+  int output = a - b;
+  return '$output, Sub $a - $b = $output';
+}
+
+// returns a message with the output of the two other functions
+String showFunc(int a, int b) {
+  String sumMessage = add(a, b).split(',').last;
+  String diffMessage = sub(a, b).split(',').last;
+  return '$sumMessage, $diffMessage';
+}
