96f033d503c115b6d0bcf932b9c95f492e9bb041
timidgeek <9liveslinds@gmail.com>
2024-02-19 11:12:12 -0600

    task 2

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,28 @@
+class Password {
+  // initialize properties
+  String _password = '';
+
+  // getter and setter for password
+  String get password => _password;
+  set password(String value) => _password = value;
+
+  // method to check if the password is valid
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) return false;
+
+    // check if it contains uppercase and lowercase letters
+    if (!RegExp(r'[a-z]').hasMatch(_password) ||
+        !RegExp(r'[A-Z]').hasMatch(_password)) return false;
+
+    // check if it contains numbers
+    if (!RegExp(r'[0-9]').hasMatch(_password)) return false;
+
+    return true;
+  }
+
+  // override toString() method
+  @override
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+}
