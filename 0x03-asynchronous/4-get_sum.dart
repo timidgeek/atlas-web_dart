f15db424e82474fda55981e13561681a18a9470e
timidgeek <9liveslinds@gmail.com>
2024-02-21 16:39:32 -0600

    task 4 baby

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import '4-util.dart';
+
+// calculates total price of items for a certain user
+Future<double> calculateTotal() async {
+  try { 
+    // get user id
+    String userData = await fetchUserData();
+    Map<String, dynamic> userDataMap = json.decode(userData);
+    String id = userDataMap['id'];
+
+    // get orders by user id
+    String userOrders = await fetchUserOrders(id);
+    List<dynamic> orders = json.decode(userOrders); // get items in list
+
+    // price items
+    double totalPrice = 0;
+    for (var item in orders) {
+      String productName = item.toString(); 
+      String productPriceData = await fetchProductPrice(productName);
+      double productPrice = double.parse(productPriceData);
+      totalPrice += productPrice;
+    }
+
+    return totalPrice;
+  } catch (e) {
+    print('error caught: $e');
+    return -1;
+  }
+}
