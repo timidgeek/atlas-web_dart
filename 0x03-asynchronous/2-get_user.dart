fa5fdc363e7ae15577c4617a49be85ee000d477f
timidgeek <9liveslinds@gmail.com>
2024-02-21 12:08:42 -0600

    handled the unhandled exception

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -2,6 +2,10 @@ import '2-util.dart';
 
 // prints the user’s string representation 
 Future<void> getUser() async {
-  String jsonData = await fetchUser();
-  print('$jsonData');
+  try {
+    String jsonData = await fetchUser();
+    print('$jsonData');
+  } catch (e) {
+    print('error caught: $e');
+  }
 }
